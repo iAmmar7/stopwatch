49b10b6e476844f71b1a0866f99f9b7c5b882f1b
Ammar <maansaridot@gmail.com>
2019-04-05 01:57:04 +0500

    sass attributes added

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,9 @@
 var second_tick = document.querySelector(".seconds-container");
 var minute_tick = document.querySelector(".minutes-container");
+timer = document.getElementById("time");
+
+pause = document.querySelector(".fa-pause");
+play = document.querySelector(".fa-play");
 
 var state = "stop";
 var miliseconds = 0;
@@ -11,6 +15,7 @@ function init(value) {
   if(value === "run") {
     second_tick.className = "seconds-container";
     minute_tick.className = "minutes-container";
+    timer.className = "timer";
     start();
   }
 
@@ -23,56 +28,6 @@ function init(value) {
   }
 }
 
-function updateState(currentState) {
-  if (currentState === "running") {
-    second_tick.style.webkitAnimationPlayState = "running";
-    minute_tick.style.webkitAnimationPlayState = "running";
-    
-    document.querySelector(".start").innerHTML = "Pause";
-    interval = setInterval(digitalTime, 1000);
-  }
-
-  if (currentState === "pause") {
-    clearInterval(interval);
-
-    second_tick.style.webkitAnimationPlayState = "paused";
-    minute_tick.style.webkitAnimationPlayState = "paused";
-
-    document.querySelector(".start").innerHTML = "Resume";
-  }
-  
-  if (currentState === "stop") {
-    clearInterval(interval);
-    miliseconds = seconds = minutes = 0;
-    displayTime(0, 0);
-
-    second_tick.style.webkitAnimationPlayState = "paused";
-    minute_tick.style.webkitAnimationPlayState = "paused";
-
-    document.querySelector(".start").innerHTML = "Start";
-  }
-}
-
-function displayTime(min, sec) {
-  if (sec < 9) {
-    sec = `0${sec}`; 
-  }
-  if (min < 9 ) {
-    min = `0${min}`
-  }
-  time = `${min}:${sec}`; 
-  console.log(time);
-  document.getElementById("time").innerHTML = time;
-}
-
-function digitalTime() {
-  seconds++;
-  if(seconds >= 60) {
-    seconds = 0;
-    minutes++;
-  }
-  displayTime(minutes, seconds);
-}
 
 function start() {
   if (state === "stop") {
@@ -108,4 +63,64 @@ function reset() {
   while (node.firstChild) {
       node.removeChild(node.firstChild);
   }
+}
+
+function updateState(currentState) {
+  if (currentState === "running") {
+    second_tick.style.webkitAnimationPlayState = "running";
+    minute_tick.style.webkitAnimationPlayState = "running";
+
+    pause.style.display = "inline";
+    play.style.display = "none";
+    
+    timer.id = "time";
+
+    interval = setInterval(digitalTime, 1000);
+  }
+
+  if (currentState === "pause") {
+    clearInterval(interval);
+
+    second_tick.style.webkitAnimationPlayState = "paused";
+    minute_tick.style.webkitAnimationPlayState = "paused";
+
+    play.style.display = "inline";
+    pause.style.display = "none";
+
+    timer.id = "changed-timer";
+  }
+  
+  if (currentState === "stop") {
+    clearInterval(interval);
+    miliseconds = seconds = minutes = 0;
+    displayTime(0, 0);
+
+    second_tick.style.webkitAnimationPlayState = "paused";
+    minute_tick.style.webkitAnimationPlayState = "paused";
+
+    play.style.display = "inline";
+    pause.style.display = "none";
+
+  }
+}
+
+function displayTime(min, sec) {
+  if (sec < 10) {
+    sec = `0${sec}`; 
+  }
+  if (min < 10) {
+    min = `0${min}`
+  }
+  time = `${min}:${sec}`; 
+  console.log(time);
+  document.getElementById("time").innerHTML = time;
+}
+
+function digitalTime() {
+  seconds++;
+  if(seconds >= 60) {
+    seconds = 0;
+    minutes++;
+  }
+  displayTime(minutes, seconds);
 }
