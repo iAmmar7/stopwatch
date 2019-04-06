c2917db1bbbf4bfe695a50f7dbcf8fc7138f8a68
Ammar <maansaridot@gmail.com>
2019-04-06 17:22:51 +0500

    local storage functionality added

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,45 @@
+var clock = document.querySelector(".clock");
+
 var second_tick = document.querySelector(".seconds-container");
 var minute_tick = document.querySelector(".minutes-container");
-timer = document.getElementById("time");
+var timer = document.getElementById("time");
 
-pause = document.querySelector(".fa-pause");
-play = document.querySelector(".fa-play");
+var pause = document.querySelector(".fa-pause");
+var play = document.querySelector(".fa-play");
+
+var reset_button = document.querySelector(".reset");
+var lap_button = document.querySelector(".lap");
+
+var parent = document.querySelector(".lap-list");
+var laps_array = [];
+var store_array;
 
 var state = "stop";
-var miliseconds = 0;
+// var miliseconds = 0;
 var seconds = 0;
 var minutes = 0;
 var interval;
+
+
+seconds = JSON.parse(localStorage.getItem('seconds'));
+minutes = JSON.parse(localStorage.getItem('minutes'));
+store_array = JSON.parse(localStorage.getItem('laps_array'));
+
+if (store_array) {
+  laps_array = store_array;
+  console.log(laps_array);
+}
+
+displayLaps();
+
+document.querySelector(".seconds").style.transform = `rotateZ(${seconds * 6}deg)`;
+document.querySelector(".minutes").style.transform = `rotateZ(${minutes * 6}deg)`;
+displayTime(minutes, seconds);
+
+if(seconds > 0 || minutes > 0) {
+  timer.id = "changed-timer";
+}
+
 
 function init(value) {
   if(value === "run") {
@@ -46,9 +76,23 @@ function start() {
 
 function lap() {
   if (state !== "stop") {
-    var li = document.createElement("LI");
-    li.innerHTML = `Minutes: ${minutes} Seconds: ${seconds}`;
-    document.querySelector(".lap-list").appendChild(li);
+    // parent.style.opacity = "1";
+
+    // clock.style.top = "28%";
+
+    // timer.style.width = "8vw";
+    // timer.style.fontSize = "3.5vw";
+
+    // document.querySelector(".clock-container").style.transform = "translateX(-10%)";
+    
+    // document.querySelector(".lap-container").style.display = "flex";
+    // document.querySelector(".lap-container").style.transform = "translateX(-40%)";
+
+    laps_array.push({Minutes: minutes, Seconds: seconds});
+    localStorage.setItem('laps_array', JSON.stringify(laps_array));
+
+    removeLaps();
+    displayLaps();
   }
 }
 
@@ -57,12 +101,10 @@ function reset() {
   updateState("stop");
   state = "stop";
 
-  second_tick.className = minute_tick.className = "changed-container";
-
-  var node = document.querySelector(".lap-list");
-  while (node.firstChild) {
-      node.removeChild(node.firstChild);
-  }
+  clearInterval(interval);
+  seconds = minutes = 0;
+  displayTime(0, 0);
+  // clock.style.top = "50%";
 }
 
 function updateState(currentState) {
@@ -72,6 +114,11 @@ function updateState(currentState) {
 
     pause.style.display = "inline";
     play.style.display = "none";
+
+    reset_button.style.visibility = "visible";
+    lap_button.style.visibility = "visible";
+    reset_button.style.opacity = "1";
+    lap_button.style.opacity = "1";
     
     timer.id = "time";
 
@@ -91,16 +138,18 @@ function updateState(currentState) {
   }
   
   if (currentState === "stop") {
-    clearInterval(interval);
-    miliseconds = seconds = minutes = 0;
-    displayTime(0, 0);
-
     second_tick.style.webkitAnimationPlayState = "paused";
     minute_tick.style.webkitAnimationPlayState = "paused";
+
+    reset_button.style.visibility = "initial";
+    lap_button.style.visibility = "initial";
+    reset_button.style.opacity = "0";
+    lap_button.style.opacity = "0";
 
     play.style.display = "inline";
     pause.style.display = "none";
 
+    timer.id = "time";
   }
 }
 
@@ -113,7 +162,17 @@ function displayTime(min, sec) {
   }
   time = `${min}:${sec}`; 
   console.log(time);
-  document.getElementById("time").innerHTML = time;
+
+  if (state === "pause") {
+    document.getElementById("changed-timer").innerHTML = time;
+  } else {
+    document.getElementById("time").innerHTML = time;
+
+    // document.getElementById("ms").innerHTML = ms;  
+
+    document.querySelector(".seconds").style.transform = `rotateZ(${sec * 6}deg)`;
+    document.querySelector(".minutes").style.transform = `rotateZ(${min * 6}deg)`;
+  }
 }
 
 function digitalTime() {
@@ -123,4 +182,30 @@ function digitalTime() {
     minutes++;
   }
   displayTime(minutes, seconds);
+}
+
+function refresh() {
+  localStorage.setItem('seconds', JSON.stringify(seconds));
+  localStorage.setItem('minutes', JSON.stringify(minutes));
+}
+
+function displayLaps() {
+  for (let i=0; i<laps_array.length; i++) {
+    var li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`Minutes: ${laps_array[i].Minutes} Seconds: ${laps_array[i].Seconds}`)
+    );
+    parent.appendChild(li);
+  }
+}
+
+function removeLaps() {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function clearLaps() {
+  localStorage.removeItem('laps_array');
+  removeLaps();
 }
