54abea4cc0f561bb20067420a152a56665db1d8f
Ammar <maansaridot@gmail.com>
2019-04-08 20:03:53 +0500

    lap difference added

--- javascript/index.js
+++ javascript/index.js
@@ -20,40 +20,43 @@ var store_array;
 
 
 var state = "stop";
-hours = 0;
-minutes = 0;
-seconds = 0;
-miliseconds = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    miliseconds = 0;
+    temp_ms= 0;
 var interval;
 
 store_hours = JSON.parse(localStorage.getItem('hours'));
 store_minutes = JSON.parse(localStorage.getItem('minutes'));
 store_seconds = JSON.parse(localStorage.getItem('seconds'));
 store_miliseconds = JSON.parse(localStorage.getItem('miliseconds'));
+store_lap_miliseconds = JSON.parse(localStorage.getItem('lap_ms'));
 store_array = JSON.parse(localStorage.getItem('laps_array'));
 
 if (store_array) {
   laps_array = store_array;
-  console.log(laps_array);
+  lap_container.style.transform = "translate(120%, -110%)";
+  lap_container.style.visibility = "visible";
+  clock.style.transform = "translateX(-80%)";
+  lap_container.style.opacity = "1";
 }
 if (store_hours) {
   hours = store_hours;
-  console.log(hours);
 }
 if (store_minutes) {
   minutes = store_minutes;
-  console.log(minutes);
 }
 if (store_seconds) {
   seconds = store_seconds;
-  console.log(seconds);
 }
 if (store_miliseconds) {
   miliseconds = store_miliseconds;
-  console.log(miliseconds);
+}
+if (store_lap_miliseconds) {
+  temp_ms = store_miliseconds;
 }
 
-console.log(hours, minutes, seconds, miliseconds);
 displayLaps();
 
 document.querySelector(".hours").style.transform = `rotateZ(${hours * 6}deg)`;
@@ -78,14 +81,14 @@ function init(value) {
     lap_container.style.transform = "translate(120%, -110%)";
     lap_container.style.visibility = "visible";
     clock.style.transform = "translateX(-80%)";
-    lap_container.style.opacity = "1"
+    lap_container.style.opacity = "1";
   }
 
   if (value === "reset") {
+    removeLaps();
     reset();
   }
 }
-
 
 function start() {
   if (state === "stop") {
@@ -104,11 +107,24 @@ function start() {
 
 function lap() {
   if (state !== "stop") {
-    laps_array.unshift({ 'Minutes': minutes, 'Seconds': seconds, 'Mili': miliseconds});
+    
+    console.log(hours, minutes, seconds, miliseconds);
+    let temp_arr = lapStep();
+    console.log(temp_arr);
+
+    laps_array.unshift({
+      'Minutes': minutes,
+      'Seconds': seconds,
+      'Mili': miliseconds,
+      'Lap_Min': temp_arr[1],
+      'Lap_Sec': temp_arr[2],
+      'Lap_Ms': temp_arr[3],
+  });
     localStorage.setItem('laps_array', JSON.stringify(laps_array));
 
     removeLapsFromPage();
     displayLaps();
+    temp_ms = 0;
   }
 }
 
@@ -117,15 +133,19 @@ function reset() {
   state = "stop";
 
   clearInterval(interval);
-  hours = minutes = seconds = miliseconds = 0;
+  hours = minutes = seconds = miliseconds = temp_ms = 0;
 
   displayTime(0, 0, 0, 0);
   removeTimeFromStorage();
+
+  lap_container.style.transform = "translate(80%, -110%)";
+  lap_container.style.visibility = "hidden";
+  clock.style.transform = "translateX(0%)";
+  lap_container.style.opacity = "0";
 }
 
 function updateState(currentState) {
   if (currentState === "running") {
-    console.log(hours, minutes, seconds, miliseconds);
 
     pause.style.display = "inline";
     play.style.display = "none";
@@ -161,7 +181,6 @@ function updateState(currentState) {
     timer.id = "time";
   }
 }
-
 function displayTime(hr, min, sec, ms) {
   if (ms < 10) {
     ms = `0${ms}`;
@@ -187,6 +206,7 @@ function displayTime(hr, min, sec, ms) {
 
 function increaseTime() {
   miliseconds++;
+  temp_ms++;
   if (miliseconds >= 60) {
     miliseconds = 0;
     seconds++;
@@ -207,6 +227,7 @@ function refresh() {
   localStorage.setItem('minutes', JSON.stringify(minutes));
   localStorage.setItem('seconds', JSON.stringify(seconds));
   localStorage.setItem('miliseconds', JSON.stringify(miliseconds));
+  localStorage.setItem('lap_ms', JSON.stringify(temp_ms));
 }
 
 function displayLaps() {
@@ -215,31 +236,65 @@ function displayLaps() {
     li.setAttribute("id", i);
     li.setAttribute("onclick", `deleteLap(${i})`);
 
-    var small = document.createElement("small");
-    small.appendChild(
+    var s_no = document.createElement("small");
+    s_no.appendChild(
       document.createTextNode(`#${i + 1}`)
     )
 
-    var p = document.createElement("p");
-    p.appendChild(
+    var p1 = document.createElement("p");
+    p1.appendChild(
       document.createTextNode(`${laps_array[i].Minutes} ${laps_array[i].Seconds}.`)
     )
 
-    var ms = document.createElement("small");
-    ms.setAttribute("class", "ms");
-    ms.appendChild(
+    var ms1 = document.createElement("small");
+    ms1.setAttribute("class", "ms");
+    ms1.appendChild(
       document.createTextNode(`${laps_array[i].Mili}`)
     )
 
-    li.appendChild(small);
-    li.appendChild(p);
-    li.appendChild(ms);
+    li.appendChild(s_no);
+    li.appendChild(p1);
+    li.appendChild(ms1);
+
+    var p2 = document.createElement("p");
+    p2.appendChild(
+      document.createTextNode(`${laps_array[i].Lap_Min} ${laps_array[i].Lap_Sec}.`)
+    )
+
+    var ms2 = document.createElement("small");
+    ms2.setAttribute("class", "ms");
+    ms2.appendChild(
+      document.createTextNode(`${laps_array[i].Lap_Ms}`)
+    )
+
+    li.appendChild(p2);
+    li.appendChild(ms2);
+
     parent.appendChild(li);
   }
 }
 
+function lapStep() {
+  // console.log(temp_ms);
+  let hours = Math.floor(temp_ms / (60*60*60));
+  let hr_rem = temp_ms % (60*60*60);
+
+  let mints = Math.floor(hr_rem / (60*60));
+  let min_rem = hr_rem % (60*60);
+
+  let sec = Math.floor(min_rem / (60));
+  let sec_rem = min_rem % (60);
+
+  let ms = sec_rem;
+
+  let arr = [hours, mints, sec, ms];
+  
+  // console.log(arr);
+
+  return arr;
+}
+
 function removeLaps() {
-  console.log(parent);
   removeLapsFromPage();
   laps_array.splice(0);
   localStorage.removeItem('laps_array');
@@ -252,7 +307,6 @@ function removeLapsFromPage() {
 }
 
 function deleteLap(id) {
-  console.log("i am " + id);
   laps_array.splice(id, 1);
   localStorage.setItem('laps_array', JSON.stringify(laps_array));
   removeLapsFromPage();
@@ -263,5 +317,5 @@ function removeTimeFromStorage() {
   localStorage.removeItem('hours');
   localStorage.removeItem('minutes');
   localStorage.removeItem('seconds');
-  localStorage.removeItem('miliseconds');
+  localStorage.removeItem('lap_ms');
 }
